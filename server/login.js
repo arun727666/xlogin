2717c49b92892d21b09fc01e5d02c56b29e1c772
arun727666 <70043711+arun727666@users.noreply.github.com>
2023-11-14 22:18:16 +0530

    Update login.js

--- server/login.js
+++ server/login.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
   const { username, password } = req.body;
 
   // Perform simple authentication (replace this with your actual logic)
-  if (username === 'DA' && password === 'ArunDhaya') {
+  if (username === 'arun' && password === '123') {
     res.status(200).json({ success: true, redirect: '/dashboard' });
   } else {
     res.status(401).json({ success: false, message: 'Invalid credentials' });
