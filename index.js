43b901d58405f6f70933727055862a5ee59a2da4
arun727666 <70043711+arun727666@users.noreply.github.com>
2023-11-15 18:29:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
